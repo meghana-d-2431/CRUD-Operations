8d33eb467bd012be0e9843716df95a52386f8a4a
Megh <meghana@caredocs.ai>
2026-04-20 16:26:23 -0500

    Created project

---
+++ operations/src/test/java/curd/operations/OperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package curd.operations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
